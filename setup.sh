30f9fd8d1e65bb740c512f2a2ac956413a173816
Mayuri955715 <mayuriii5625@gmail.com>
2026-07-15 05:08:37 +0530

    Restore setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-echo "Updating packages..."
-
 sudo apt update -y
-
-echo "Installing Nginx..."
-
 sudo apt install nginx -y
 
-echo "Starting Nginx..."
-
 sudo systemctl start nginx
-
-echo "Enabling Nginx..."
-
 sudo systemctl enable nginx
-
-echo "Copying Website..."
 
 sudo cp index.html /var/www/html/index.html
 
-echo "Website deployed successfully!"
+echo "Deployment completed successfully!"
